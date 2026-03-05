5e2d9f2b2b46612133662655557c526cb9770178
Ronald McIntyre <59479270+Mcintyre-r@users.noreply.github.com>
2026-03-04 19:35:28 -0500

    Update redirect URL and link in MitPage

--- app/mit/page.tsx
+++ app/mit/page.tsx
@@ -7,14 +7,14 @@ export default function MitPage() {
 
 
   useEffect(() => {
-    window.location.href = 'https://docs.google.com/spreadsheets/d/13rn9CxeB4ovSkxG5-IgpBzXNF3uFQ4RTbrxmi-WMF_o/edit?usp=sharing';
+    window.location.href = 'https://docs.google.com/spreadsheets/d/1FmG8NL2DybieW9FuZrPF7w6iABFtGVEzwwoFPCjslZc/htmlview?gid=161526414&pru=AAABm_2EK_E*ZJjdxnP6OrMGY7fZM7ufwA#gid=1324709415';
   }, []);
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="text-center">
         <h1 className="text-2xl font-semibold mb-4">Redirecting to Mitigation Sheet...</h1>
-        <p className="text-gray-600">If you are not redirected automatically, <a href="https://docs.google.com/spreadsheets/d/13rn9CxeB4ovSkxG5-IgpBzXNF3uFQ4RTbrxmi-WMF_o/edit?usp=sharing" className="text-blue-600 hover:underline">click here</a>.</p>
+        <p className="text-gray-600">If you are not redirected automatically, <a href="https://docs.google.com/spreadsheets/d/1FmG8NL2DybieW9FuZrPF7w6iABFtGVEzwwoFPCjslZc/htmlview?gid=161526414&pru=AAABm_2EK_E*ZJjdxnP6OrMGY7fZM7ufwA#gid=1324709415" className="text-blue-600 hover:underline">click here</a>.</p>
       </div>
     </div>
   );
